51e3c01893a92da0a9e785850e1f8ab6854a1a63
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-02-21 21:31:39 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -11,3 +11,7 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 12) == NORMAL);
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 12) == NORMAL);
 }
+TEST_CASE("infers the breach according to alerter") {
+  REQUIRE(checkAndAlert(TO_CONTROLLER,HI_ACTIVE_COOLING,12));
+
+}
