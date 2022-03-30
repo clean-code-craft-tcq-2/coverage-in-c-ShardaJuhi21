fc5beb84a950b4dbbae425643dddef77a04644f4
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-03-30 21:10:14 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -8,9 +8,9 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
   REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
   REQUIRE(inferBreach(25, 20, 30) == NORMAL);
-  //REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 12) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 12) == TOO_LOW);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 12) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 12) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 12) == TOO_LOW);
 }
 // TEST_CASE("infers the breach according to alerter") {
 // REQUIRE(checkAndAlert(TO_CONTROLLER,TestBatteryCharacter,12));
